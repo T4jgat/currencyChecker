799a09bc03895f42ba89f25b31100715750b06b3
talga <tmtmtm1423@gmail.com>
2023-07-06 02:32:20 +0600

    branch init commit

--- src/main/java/kz/exchangeCourse/usdkzt/CurrencyCheckerApplication.java
+++ src/main/java/kz/exchangeCourse/usdkzt/CurrencyCheckerApplication.java
@@ -3,6 +3,7 @@ package kz.exchangeCourse.usdkzt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// t4jgat branch
 @SpringBootApplication
 public class CurrencyCheckerApplication {
 
