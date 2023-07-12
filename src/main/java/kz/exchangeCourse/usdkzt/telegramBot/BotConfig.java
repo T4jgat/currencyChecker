67a8711f082a1a86783c8b97b267759030d0bb27
talga <tmtmtm1423@gmail.com>
2023-07-12 14:55:02 +0600

    Added ReplyKeyboard

--- src/main/java/kz/exchangeCourse/usdkzt/telegramBot/BotConfig.java
+++ src/main/java/kz/exchangeCourse/usdkzt/telegramBot/BotConfig.java
@@ -2,6 +2,7 @@ package kz.exchangeCourse.usdkzt.telegramBot;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -13,4 +14,14 @@ public class BotConfig {
     String botName;
     @Value("${bot.token}")
     String token;
+
+    @Bean
+    public mPage pages() {
+        return mPage.FIRST_PAGE;
+    }
+
+    public enum mPage{
+        FIRST_PAGE, SECONG_PAGE, THIRD_PAGE;
+    }
+
 }
